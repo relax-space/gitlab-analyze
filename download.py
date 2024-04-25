c9f18dc28ff7b97cdcc5f9ffe3932c2b42504062
xiaoxinmiao <xiaoxm_001@outlook.com>
2024-04-25 16:00:22 +0800

    download

--- download.py
+++ download.py
@@ -1,91 +1,87 @@
-import os
-import shutil
+from os import path as os_path
 import git
 from asyncio import Semaphore, create_task, gather, run as asyncio_run
 from aiohttp import ClientSession, BasicAuth
 from util import PERSONAL_ACCESS_TOKEN, LOCAL_PATH, GITLAB_BASE_URL, MAX_REQUESTS
-from json import dump
+from json import dump, load as json_load
 from datetime import datetime
 
 
-async def get_projects(url):
+async def get_projects(url, session, semaphore):
     headers = {"Private-Token": PERSONAL_ACCESS_TOKEN}
+    try:
+        async with semaphore:
+            async with session.get(url, headers=headers) as response:
+                data = await response.json()
+                return data
+    except Exception as e:
+        print(url, e)
+        return None
+        pass
 
-    async with ClientSession(auth=BasicAuth("user", PERSONAL_ACCESS_TOKEN)) as session:
-        async with session.get(url, headers=headers) as response:
-            data = await response.json()
-            return data
+
+def get_path_namespace(local_path, username, full_path):
+    project_name = full_path.split("/")
+    project_path = os_path.join(local_path, username, *project_name)
+    return project_path
 
 
-async def clone_or_pull_project(i, project, start_date):
-    project_name_one: str = project["path_with_namespace"]
-    project_name = project_name_one.split("/")
+async def clone_or_pull_project(username, project, start_date):
+    project_name: str = project["path_with_namespace"]
     project_url = project["http_url_to_repo"]
-    project_path = os.path.join(LOCAL_PATH, *project_name)
+    project_path = get_path_namespace(LOCAL_PATH, username, project_name)
     updated_at = datetime.strptime(project["last_activity_at"], "%Y-%m-%dT%H:%M:%S.%fZ")
     if updated_at >= datetime.strptime(start_date, "%Y-%m-%d"):
-        if os.path.exists(project_path):
+        if os_path.exists(project_path):
             try:
                 repo = git.Repo(project_path)
                 repo.remotes.origin.pull()
-
             except Exception as e:
-                print(f"{i}.error {project_name}")
+                print(f"error {project_name}")
                 return "-1"
             else:
-                print(f"{i}.updated {project_name}")
+                print(f"updated {project_name}")
         else:
             git.Repo.clone_from(project_url, project_path)
-            print(f"{i}.cloned {project_name}")
-        return project_name_one
+            print(f"cloned {project_name}")
+        return project_name
     else:
-        print(f"find it {project_name}")
-        return "-2"
+        return "-1"
 
 
-async def main(start_date):
+def get_project_names(file_name):
+    with open(file_name, mode="r", encoding="utf8") as f:
+        resp = json_load(f)
+        return resp
+
+
+async def main(start_date, file_name):
     semaphore = Semaphore(MAX_REQUESTS)
     project_all_names = []
-    page = 1
-    has_next_page = True
-    async with semaphore:
-        while has_next_page:
-            url = f"{GITLAB_BASE_URL}/projects?page={page}&per_page=10&order_by=last_activity_at&sort=desc"
-            projects = await get_projects(url)
-            project_names = [project["path_with_namespace"] for project in projects]
-            project_all_names.append(project_names)
-            print(f"项目总数(分页{page})： {len(projects)}个:{project_names}")
+    dic_project: dict = get_project_names(file_name)
+    # http://10.0.50.12:8088/api/v4/projects?simple=true&search_namespaces=true&search=bsn-ddc/ddchashrate/ddc-hashrate-web
+    async with ClientSession(auth=BasicAuth("user", PERSONAL_ACCESS_TOKEN)) as session:
+        for k, v in dic_project.items():
+            projects = v
+            tasks = []
+            for pj in projects:
+                url = f"{GITLAB_BASE_URL}/projects?simple=true&search_namespaces=true&search={pj}"
+                task = create_task(get_projects(url, session, semaphore))
+                tasks.append(task)
+            resp_projects = await gather(*tasks)
             tasks = []
             exp_set = set()
-            if projects:
-                for i, project in enumerate(projects):
+            if resp_projects:
+                for i, project in enumerate(resp_projects):
                     exp_set.add(project["path_with_namespace"])
-                    task = create_task(clone_or_pull_project(i, project, start_date))
+                    task = create_task(clone_or_pull_project(k, project, start_date))
                     tasks.append(task)
 
                 result = await gather(*tasks)
                 act_set = set(result)
                 if exp_set != act_set:
-                    err_set = exp_set - act_set
-                    for i in err_set:
-                        project_name = i.split("/")
-                        project_url = project["http_url_to_repo"]
-                        project_path = os.path.join(LOCAL_PATH, *project_name)
-                        if os.path.isdir(project_path):
-                            try:
-                                shutil.rmtree(project_path)
-                            except Exception as e:
-                                print(f"{i}.error2 {project_name}")
-                            else:
-                                git.Repo.clone_from(project_url, project_path)
-                                print(f"{i}.cloned {project_name}")
-
-                if "-2" in act_set:
-                    break
+                    print(f"{k} 以下项目需要手动下载：{exp_set-act_set}")
                 pass
-            else:
-                has_next_page = False
-            page += 1
 
     with open("projects.txt", mode="w", encoding="utf8") as f:
         dump(project_all_names, f)
@@ -93,4 +89,5 @@ async def main(start_date):
 
 if __name__ == "__main__":
     start_date = "2024-01-01"
-    asyncio_run(main(start_date))
+    file_name = "raw20240425.json"
+    asyncio_run(main(start_date, file_name))
